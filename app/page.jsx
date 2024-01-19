7b53af80326ea5d3fd44af3ca47565bc92ab1131
Abhinav Thota <abhinavthota01@gmail.com>
2024-01-19 14:21:08 -0800

    Learnt about client/server components and why it matters.

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,11 @@
-"use client"
-import { useState } from 'react';
- 
+import LikeButton from './LikeButton'
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
  
 export default function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
- 
-  const [likes, setLikes] = useState(0);
- 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
- 
+
   return (
     <div>
       <Header title="Develop. Preview. Ship." />
@@ -22,8 +14,7 @@ export default function HomePage() {
           <li key={name}>{name}</li>
         ))}
       </ul>
- 
-      <button onClick={handleClick}>Like ({likes})</button>
+      <LikeButton/>
     </div>
   );
 }
